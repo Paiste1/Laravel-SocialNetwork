9c252df101f60e69e8461d4fafd104dbfb4b4876
Paiste1 <alexpaiste.ap@gmail.com>
2021-03-29 13:22:31 +0800

    реализовал добавление в друзья

--- app/User.php
+++ app/User.php
@@ -69,4 +69,20 @@ class User extends Authenticatable
     {
         return "https://www.gravatar.com/avatar/{{ md5($this->email)?d=mp&s=40 }}";
     }
+
+    public function friendsOfMine()
+    {
+        return $this->belongsToMany('App\User', 'friends', 'user_id', 'friend_id');
+    }
+
+    public function friendOf()
+    {
+        return $this->belongsToMany('App\User', 'friends', 'friend_id', 'user_id');
+    }
+
+    public function friends()
+    {
+        return $this->friendsOfMine()->wherePivot('accepted', true)->get()
+            ->merge( $this->friendOf()->wherePivot('accepted', true)->get() );
+    }
 }
--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -3,8 +3,19 @@
 @section('content')
     <div class="row">
         <div class="col-lg-6">
-
             @include('user.part.userblock')
+        </div>
+
+        <div class="col-lg-4 col-lg-offset-3">
+            <h4>Друзья ({{ $user->friends()->count() ?: 0 }})</h4>
+
+            @if(!$user->friends()->count())
+                <p>нет друзей</p>
+            @else
+                @foreach($user->friends() as $user)
+                    @include('user.part.userblock')
+                @endforeach
+            @endif
 
         </div>
     </div>
