66aa39d00b46fb23ce7c98bac3bb2860bef72463
Paiste1 <alexpaiste.ap@gmail.com>
2021-03-30 16:25:48 +0800

    поменял роут авторизированного пользователя, сделал страницу стены, создал миграцию таблицы стена

---
+++ resources/views/timeline/index.blade.php
@@ -0,0 +1,22 @@
+@extends('templates.default')
+
+@section('content')
+
+    <div class="row">
+        <div class="col-lg-6">
+            <form action="#" method="POST">
+                <div class="form-group">
+                    <textarea class="form-control" placeholder="Чего нового?" rows="3"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Опубликовать</button>
+            </form>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-5">
+
+        </div>
+    </div>
+
+@endsection
